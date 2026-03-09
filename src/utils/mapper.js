54ffc3ea166021b4ca1d8efa0a110f2cd1d7ff4b
estevaoMG <estevao.gouveia@hotmail.com>
2026-03-09 15:59:59 -0300

    Implement order data mapping transformation

---
+++ src/utils/mapper.js
@@ -0,0 +1,14 @@
+function mapOrder(data) {
+  return {
+    orderId: data.numeroPedido,
+    value: data.valorTotal,
+    creationDate: new Date(data.dataCriacao),
+    items: data.items.map(item => ({
+      productId: Number(item.idItem),
+      quantity: item.quantidadeItem,
+      price: item.valorItem
+    }))
+  };
+}
+
+module.exports = mapOrder;
