aa1f266fca78c9dde436026c9b21bae448a9e97c
estevaoMG <estevao.gouveia@hotmail.com>
2026-03-09 17:04:31 -0300

    fix: corrigindo rotas e middleware de autenticação em orderRoutes

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,27 +2,40 @@ const express = require("express");
 const router = express.Router();
 
 const orderController = require("../controllers/orderController");
+const auth = require("../middleware/authMiddleware");
+const validate = require("../middleware/validateMiddleware");
+const { orderSchema } = require("../validations/orderValidation");
 
-router.post("/order", orderController.createOrder);
+router.post(
+  "/order",
+  auth,
+  validate(orderSchema),
+  orderController.createOrder
+);
 
-router.get("/order/list", orderController.getAllOrders);
+router.get(
+  "/order/list",
+  auth,
+  orderController.getAllOrders
+);
 
-router.get("/order/:id", orderController.getOrder);
+router.get(
+  "/order/:id",
+  auth,
+  orderController.getOrder
+);
 
-router.put("/order/:id", orderController.updateOrder);
+router.put(
+  "/order/:id",
+  auth,
+  validate(orderSchema),
+  orderController.updateOrder
+);
 
-router.delete("/order/:id", orderController.deleteOrder);
+router.delete(
+  "/order/:id",
+  auth,
+  orderController.deleteOrder
+);
 
 module.exports = router;
-
-const auth = require("../middleware/authMiddleware");
-
-router.post("/order", auth, validate(orderSchema), orderController.createOrder);
-
-router.get("/order/:id", auth, orderController.getOrder);
-
-router.get("/order/list", auth, orderController.getAllOrders);
-
-router.put("/order/:id", auth, orderController.updateOrder);
-
-router.delete("/order/:id", auth, orderController.deleteOrder);
